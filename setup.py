40df47b97d955b2f9a981c00bad43b910f3db726
ai21m027 <ai21m027@technikum-wien.at>
2022-03-29 00:06:48 +0200

    Possible automatic test fix 2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Content_Creaitor',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author=' ai21m027 ',
+    author_email=' ai21m027@technikum-wien.at',
+    description=''
+)

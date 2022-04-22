4194aaaddc29ec5fda6538035bf37acb461a0dbc
Arapsih Güngör <a.guengoer@austrian-standards.at>
2022-04-22 11:26:06 +0200

    adopt test_StripPunctuation.py

--- tests/test_StripPunctuation.py
+++ tests/test_StripPunctuation.py
@@ -3,15 +3,16 @@ import pandas as pd
 from src.pipeline.StripPunctuation import StripPunctuation
 from src.pipeline.PipelineElement import PipelineElement
 
+
 class TestStripPunctuation():
     def test_class_inheritance(self):
-         object = StripPunctuation()
-         assert issubclass(PipelineElement,PipelineElement)
+        assert issubclass(StripPunctuation, PipelineElement)
+
     def test_punctuation_stripping(self):
         object = StripPunctuation()
-        data = pd.DataFrame(['test1!#$%&()*+,-./:;<=>?@[\\]^_`{|}~','test2'])
+        data = pd.DataFrame(['test1!#$%&()*+,-./:;<=>?@[\\]^_`{|}~', 'test2'])
         data.columns = ['ProcessedSubtitles']
         data = object.process(data)
-        data_compare = pd.DataFrame(['test1','test2'])
+        data_compare = pd.DataFrame(['test1', 'test2'])
         data_compare.columns = ['ProcessedSubtitles']
         assert data_compare.equals(data)

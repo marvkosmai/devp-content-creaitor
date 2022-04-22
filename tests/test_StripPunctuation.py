b04fe34b78095ca0b2334f25558e9ef5aa356060
Arapsih Güngör <a.guengoer@austrian-standards.at>
2022-04-22 11:27:53 +0200

    Revert "adopt test_StripPunctuation.py"
    This reverts commit 4194aaaddc29ec5fda6538035bf37acb461a0dbc.

--- tests/test_StripPunctuation.py
+++ tests/test_StripPunctuation.py
@@ -3,16 +3,15 @@ import pandas as pd
 from src.pipeline.StripPunctuation import StripPunctuation
 from src.pipeline.PipelineElement import PipelineElement
 
-
 class TestStripPunctuation():
     def test_class_inheritance(self):
-        assert issubclass(StripPunctuation, PipelineElement)
-
+         object = StripPunctuation()
+         assert issubclass(PipelineElement,PipelineElement)
     def test_punctuation_stripping(self):
         object = StripPunctuation()
-        data = pd.DataFrame(['test1!#$%&()*+,-./:;<=>?@[\\]^_`{|}~', 'test2'])
+        data = pd.DataFrame(['test1!#$%&()*+,-./:;<=>?@[\\]^_`{|}~','test2'])
         data.columns = ['ProcessedSubtitles']
         data = object.process(data)
-        data_compare = pd.DataFrame(['test1', 'test2'])
+        data_compare = pd.DataFrame(['test1','test2'])
         data_compare.columns = ['ProcessedSubtitles']
         assert data_compare.equals(data)
